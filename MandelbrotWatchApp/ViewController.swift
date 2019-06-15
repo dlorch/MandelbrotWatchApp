cceb876ee4c06f744b0fabcc78f107f05dc3f659
Daniel Lorch <dlorch@gmail.com>
2019-06-15 19:16:27 +0200

    Initial Commit

---
+++ MandelbrotWatchApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MandelbrotWatchApp
+//
+//  Created by Daniel Lorch on 15.06.19.
+//  Copyright © 2019 Daniel Lorch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
